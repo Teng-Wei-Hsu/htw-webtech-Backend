17bb4e017729b1b3c114c1cff1948df4341b827e
Teng-Wei-Hsu <s0595751@htw-berlin.de>
2025-11-14 13:07:32 +0100

    Update controller

--- src/main/java/htw/webtech/myapp/rest/controller/Controller.java
+++ src/main/java/htw/webtech/myapp/rest/controller/Controller.java
@@ -2,12 +2,14 @@ package htw.webtech.myapp.rest.controller;
 
 
 import htw.webtech.myapp.rest.model.Restaurant;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class Controller {
 
     @GetMapping("/restaurants")
