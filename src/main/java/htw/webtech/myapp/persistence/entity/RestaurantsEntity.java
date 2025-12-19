a757b87a8bd1cf4e61171b527c544ca259a07908
Teng-Wei-Hsu <s0595751@htw-berlin.de>
2025-12-19 11:11:36 +0100

    M4-Backend V9

--- src/main/java/htw/webtech/myapp/persistence/entity/RestaurantsEntity.java
+++ src/main/java/htw/webtech/myapp/persistence/entity/RestaurantsEntity.java
@@ -33,4 +33,27 @@ public class RestaurantsEntity {
         this.reviews = reviews;
     }
 
+    // ADD THESE GETTERS & SETTERS
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCountry() { return country; }
+    public void setCountry(String country) { this.country = country; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getCuisineType() { return cuisineType; }
+    public void setCuisineType(String cuisineType) { this.cuisineType = cuisineType; }
+
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
+
+    public List<String> getReviews() { return reviews; }
+    public void setReviews(List<String> reviews) { this.reviews = reviews; }
+
+
 }
