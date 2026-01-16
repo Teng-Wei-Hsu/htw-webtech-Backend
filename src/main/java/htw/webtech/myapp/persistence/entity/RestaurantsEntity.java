acd5d896cb8af21a09e4d4549eba3fcbc29a8b0e
Teng-Wei-Hsu <s0595751@htw-berlin.de>
2026-01-16 10:04:13 +0100

    test

--- src/main/java/htw/webtech/myapp/persistence/entity/RestaurantsEntity.java
+++ src/main/java/htw/webtech/myapp/persistence/entity/RestaurantsEntity.java
@@ -35,30 +35,30 @@ public class RestaurantsEntity {
         this.favorite = false; // explicit default
     }
 
-    // ADD GETTERS & SETTERS
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+// ADD GETTERS & SETTERS
+public Long getId() { return id; }
+public void setId(Long id) { this.id = id; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+public String getName() { return name; }
+public void setName(String name) { this.name = name; }
 
-    public String getCountry() { return country; }
-    public void setCountry(String country) { this.country = country; }
+public String getCountry() { return country; }
+public void setCountry(String country) { this.country = country; }
 
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
+public String getCity() { return city; }
+public void setCity(String city) { this.city = city; }
 
-    public String getCuisineType() { return cuisineType; }
-    public void setCuisineType(String cuisineType) { this.cuisineType = cuisineType; }
+public String getCuisineType() { return cuisineType; }
+public void setCuisineType(String cuisineType) { this.cuisineType = cuisineType; }
 
-    public double getRating() { return rating; }
-    public void setRating(double rating) { this.rating = rating; }
+public double getRating() { return rating; }
+public void setRating(double rating) { this.rating = rating; }
 
-    public List<String> getReviews() { return reviews; }
-    public void setReviews(List<String> reviews) { this.reviews = reviews; }
+public List<String> getReviews() { return reviews; }
+public void setReviews(List<String> reviews) { this.reviews = reviews; }
 
-    public boolean isFavorite() { return favorite; }
-    public void setFavorite(boolean favorite) { this.favorite = favorite; }
+public boolean isFavorite() { return favorite; }
+public void setFavorite(boolean favorite) { this.favorite = favorite; }
 
 
 }
